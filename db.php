035415db5dfe8c22e8bd79a217c5bc27bac100cd
Dimanrik <19971225@mail.ru>
2017-07-05 21:53:47 +0400

    fffffffff

--- db.php
+++ db.php
@@ -1,10 +1,9 @@
 <?php
 $host = '127.0.0.1';
-$db   = 'Pugs';
+$db = 'Todo';
 $user = 'root';
 $pass = '';
 $charset = 'utf8';
-
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $opt = [
@@ -14,16 +13,61 @@ $opt = [
 ];
 $pdo = new PDO($dsn, $user, $pass, $opt);
 
-if ($_POST['name']!==null & $_POST['color']!==null)
+if ($_POST['record']!==null)
 {
-  $sql = "INSERT INTO database_of_pug_clones (Name, Color) VALUES (:name, :color)";
+  $sql = "INSERT INTO Record_table (id_of_list, Record) VALUES (:id_of_list, :record)";
 
   $stmt = $pdo->prepare($sql);
-  $stmt->execute(['name'=> $_POST['name'], 'color'=> $_POST['color']]);
-  $id = $pdo->lastInsertId();
-  $query = $pdo->query("SELECT * FROM database_of_pug_clones where id = '$id'");
+  $stmt->execute(['id_of_list'=>$_POST['id_of_list'], 'record'=>$_POST['record']]);
+
+  $last_id  = $pdo->lastInsertId();
+  $query = $pdo->query("SELECT * FROM Record_table WHERE id = '$last_id'");
   $last_record = $query->fetch();
-  echo json_encode(['id'=> $last_record['id'], 'name'=>$last_record['Name'], 'color'=>$last_record['Color']]);
+  echo json_encode(['id'=>$last_record['id'], id_of_list=>$last_record['id_of_list'], 'Record'=>$last_record['Record']]);
 }
 
-?>
+if($_POST['name_of_list'] !== null)
+{
+  $sql = "INSERT INTO List_table (Title) VALUES (:title)";
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute(['title'=>$_POST['name_of_list']]);
+
+  $last_id  = $pdo->lastInsertId();
+  $query = $pdo->query("SELECT * FROM List_table WHERE id = '$last_id'");
+  $last_record = $query->fetch();
+  echo json_encode(['id'=>$last_record['id'],  'Title'=>$last_record['Title']]);
+}
+
+if($_POST['change_name_list'] !== null)
+{
+  $sql = "UPDATE List_table SET Title=? WHERE id=?";
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute([$_POST['change_name_list'], $_POST['id_of_list']]);
+}
+
+if($_POST['id_for_delete_list']!=="")
+{
+  $sql = "DELETE from List_table where id=?";
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute([$_POST['id_for_delete_list']]);
+
+  $sql = $sql = "DELETE from Record_table where id_of_list=?";
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute([$_POST['id_for_delete_list']]);
+}
+
+if($_POST['id_for_delete_record']!=="")
+{
+  $sql = "DELETE FROM Record_table where id=?";
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute([$_POST['id_for_delete_record']]);
+}
+
+if($_POST['record_for_change']!=="")
+{
+
+  $sql = "UPDATE Record_table SET Record=? where id=?";
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute([$_POST['record_for_change'], $_POST['id_of_record']]);
+}
+ ?>
--- main.js
+++ main.js
@@ -1,36 +1,157 @@
 $(document).ready(function()
 {
-  $("#form1").submit(function(event)
+  $("body").on("submit", ".add", function(e)
   {
     event.preventDefault();
-    if ($(".name").val()!=="" &  $(".color").val()!=="")
+
+    var this_input_area = $(this);
+
+    if($(this).find('.record').val()!=="")
     {
       $.ajax
       ({
-        type:"POST",
-        url: "db.php",
+        type : "POST",
+        url : "db.php",
         data:
         {
-          name: $(".name").val(),
-          color: $(".color").val()
+          record : $(this).find(".record").val(),
+          id_of_list : $(this).find(".id_of_list").val()
         },
-        success: function(data)
+        success : function(data)
         {
-          var anys = JSON.parse(data);
-          var id = anys.id;
-          console.log(id);
-           $('tbody').append("<tr><th>" + anys.id + "<th>" + anys.name + "<th>"+ anys.color + "</tr>")
-
+          var last_record = JSON.parse(data);
+          var temp_record = $(".div_hidden_record").find(".hidden_record").clone().removeClass("hidden_record");
+          temp_record.find(".id_of_record").val(last_record.id);
+          temp_record.find(".recording_cell").prepend(last_record.Record);
+          this_input_area.parents(".list").find("tbody").append(temp_record);
+          $(".record").val('');
         }
-      })
-      .done(function()
-      {
-        alert("Данные получены, скоро мопсик родится");
       })
     }
     else
     {
-      alert("Не все поля заполнены")
+      alert("Цель не введена")
+    }
+  })
+
+  $("#area_add_list").submit(function(event)
+  {
+    event.preventDefault();
+
+    if($(".name_of_list").val()!=="")
+    {
+      $.ajax
+      ({
+        type : "POST",
+        url : "db.php",
+        data:
+        {
+          name_of_list : $(".name_of_list").val()
+        },
+        success : function(data)
+        {
+          var last_list = JSON.parse(data);
+          var temp_list = $(".hidden_list").clone().removeClass("hidden_list").addClass("list");
+          temp_list.find(".notebook").after("<h5>" + last_list.Title + "</h5>");
+          temp_list.find(".add").prepend("<input type='hidden' name='id_of_list' class='id_of_list' value=" + last_list.id +">");
+          console.log(data);
+          temp_list.appendTo(".area_for_list");
+          $(".name_of_list").val('');
+
+        }
+      })
+    }
+    else
+    {
+      alert("Введите название нового списка задач");
+    }
+  })
+
+
+
+  $("body").on("click", ".button_change_title", function(e)
+  {
+    var  temp_name_of_title = $(this).parents('.list').find('h5').text();
+    $(this).parents(".list").find('h5').replaceWith('<input class="change_name_list blink" type="text">');
+    $(this).parents(".list").find('.change_name_list').val(temp_name_of_title);
+  })
+  $('body').on("keypress",".change_name_list", function(e)
+  {
+    if(e.keyCode==13)
+    {
+      if($(".change_name_list").val()!=="")
+      {
+        $.ajax
+        ({
+          type:"POST",
+          url:"db.php",
+          data:
+          {
+            change_name_list: $(".change_name_list").val(),
+            id_of_list: $(this).parents(".list").find('.id_of_list').val()
+          }
+        })
+        $(this).parents(".list").find('.change_name_list').replaceWith("<h5>" + $('.change_name_list').val() + "</h5>");
+      }
+
+    }
+  })
+
+  $("body").on("click", ".button_delete_list", function(e)
+  {
+    $.ajax
+    ({
+      type : "POST",
+      url : "db.php",
+      data :
+      {
+        id_for_delete_list : $(this).parents(".list").find('.id_of_list').val()
+      }
+    })
+    console.log($(this).parents('.list'));
+    $(this).parents('.list').remove();
+  })
+
+  $("body").on("click", ".button_delete_record", function(e)
+  {
+    console.log($(this).parents("tr"))
+    $.ajax
+    ({
+      type : "POST",
+      url : "db.php",
+      data :
+      {
+        id_for_delete_record: $(this).parents("tr").find(".id_of_record").val()
+      }
+    })
+    $(this).parents("tr").remove();
+  })
+
+
+  $("body").on("click", ".button_change_record", function(e)
+  {
+    var temp_record = $(this).parents("tr").find(".recording_cell").text();
+    $(this).parents("tr").find(".recording_cell").replaceWith('<th class="recording_cell"><input class="input_change_record blink_for_gray" type="text"></th>');
+    $(this).parents("tr").find(".input_change_record").val(temp_record);
+    click_check_for_record = false;
+  })
+
+  $("body").on("keypress", ".input_change_record", function(e)
+  {
+    if(e.keyCode==13)
+    {
+      $.ajax
+      ({
+        type : "POST",
+        url : "db.php",
+        data :
+        {
+          id_of_record : $(this).parents("tr").find(".id_of_record").val(),
+          record_for_change : $(this).parents("tr").find(".input_change_record").val()
+        }
+      })
+      $(this).parents("tr").find('.input_change_record').replaceWith($(this).parents("tr").find('.input_change_record').val());
+      click_check_for_record=true;
     }
   })
 })
